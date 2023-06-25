7a638824d45c7c00fe8bae49c3187ec55ac66d81
JackitudilinksG <73828512+JackitudilinksG@users.noreply.github.com>
2023-06-25 20:33:13 +0400

    can somewhat pull from firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -10,112 +14,53 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot){
-        if(snapshot.connectionState ==  ConnectionState.waiting){
-          return const MaterialApp(
-            home: Scaffold(
-              body: Center(
-                child: Text('pride is ending',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-            ),
-          );
-        }
-
-        else if(snapshot.hasError){
-          return const MaterialApp(
-            home: Scaffold(
-              body: Center(
-                child: Text('child should not be inside children',
-                  style: TextStyle(
-                      color: Colors.green,
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-            ),
-          );
-        }
-
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'some shit',
-          theme: ThemeData(
-            scaffoldBackgroundColor: Colors.black,
-            primarySwatch: Colors.blue,
-          ),
-          home: const Scaffold(
-            body: Center(
-              child: Text('working ig',
-                style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold
-                ),
-              ),
-            ),
-          ),
-        );
-      }
+    return MaterialApp(
+      home: GetStudentName(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+class GetStudentName extends StatelessWidget {
+  final String documentId;
 
-class _MyHomePageState extends State<MyHomePage> {
+  GetStudentName(this.documentId);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Home Page'),
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('members').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          }
+    CollectionReference members = FirebaseFirestore.instance.collection('members');
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Text('Loading...');
-          }
+    return FutureBuilder<DocumentSnapshot>(
+      //Fetching data from the documentId specified of the student
+      future: members.doc(documentId).get(),
+      builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        //Error Handling conditions
+        if (snapshot.hasError) {
+          return const Text("Something went wrong");
+        }
 
-          return ListView(
-            children: snapshot.data!.docs.map((DocumentSnapshot document) {
-              Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-              return ListTile(
-                title: Text(data['Name']),
-                subtitle: Text(data['Number']),
-                // Add more fields as needed
-              );
-            }).toList(),
-          );
-        },
-      ),
+        if (snapshot.hasData && !snapshot.data!.exists) {
+          return const Text("Document does not exist");
+        }
+
+        //Data is output to the user
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+          return ListTile(
+            title: Text(data['Name']),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(data['Number']),
+                Text(data['DOB']),
+              ],
+            ),
+          )
+        }
+
+        return const Text("Loading...");
+      },
     );
   }
 }
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
